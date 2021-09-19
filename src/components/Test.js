14ff300246eefac8f1714ddf74282791a74f1a5e
sajibsarker-1104003 <sajibpranto600@gmail.com>
2021-09-19 23:52:56 +0600

    React Hooks:3.Rules of Using Hooks

--- src/components/Test.js
+++ src/components/Test.js
@@ -3,21 +3,36 @@ import React ,{useState} from "react";
 const Test=props =>{
  
   const [myName,setMyName]=useState("");
-  const [myInfo,setMyInfo]=useState({});
+  //const [myInfo,setMyInfo]=useState({});
+  //const [myInfo,setMyInfo]=useState("My Info String ");
+  //const [myInfo,setMyInfo]=useState([1,2,3,4,5]);
+  const [myInfo,setMyInfo]=useState({
+    a:"Hello",
+    b:"World"
+  });
   const changeState=()=>{
     setMyName("Hello Sajib");
+    // setMyInfo({
+    //   age:30,
+    //   address:"Dhaka,BD"
+    // })
+
+    //setMyInfo(myInfo.concat(6));
+
     setMyInfo({
-      age:30,
-      address:"Dhaka,BD"
-    })
+         ...myInfo,
+        //  a:"Hello",
+        //  b:"Pranto"
+       })
   }
   
   return(
     <div>
       <button onClick={changeState}>SHOW</button>
       <h3>{myName}</h3>
-      <h4>{myInfo.age}</h4>
-      <h4>{myInfo.address}</h4>
+      <h4>{myInfo.a}</h4>
+      <h4>{myInfo.b}</h4>
+      
 
     </div>
     );
